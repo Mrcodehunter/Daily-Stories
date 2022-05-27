2758ece420fc5f0539224008b7834b67714edfd8
Murad <muradhossen5267@gmail.com>
2022-05-27 17:42:41 +0600

    driver class added. table model under driver class

--- database/mysql.js
+++ database/mysql.js
@@ -1,21 +1,23 @@
-const dbConfig = require("../config/mysqlConfig.js");
-const Sequelize = require("sequelize");
-const db = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,{
-    host : dbConfig.HOST,
-    dialect : dbConfig.dialect,
-});
+// const dbConfig = require("../config/mysqlConfig.js");
+// const Sequelize = require("sequelize");
+
+// const db = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,{
+//     host : dbConfig.HOST,
+//     dialect : dbConfig.dialect,
+// });
 
 
-(async ()=>{
-    try {
-      await db.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  })()
+// (async ()=>{
+//     try {
+//       await db.authenticate();
+//       console.log('Connection has been established successfully.');
+//     } catch (error) {
+//       console.error('Unable to connect to the database:', error);
+//     }
+//   })()
+  
 
 
-module.exports = db;
+// module.exports = db;
 
 
--- models/storyModel.js
+++ models/storyModel.js
@@ -1,19 +1,19 @@
-const Sequelize = require("sequelize");
-const db = require("../database/mysql");
-const story = db.define("story", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-  },
-  description: {
-    type: Sequelize.STRING,
-  },
-  author: {
-    type: Sequelize.STRING,
-  },
-});
-module.exports = story;
+// const Sequelize = require("sequelize");
+// const db = require("../database/mysql");
+// const story = db.define("story", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   title: {
+//     type: Sequelize.STRING,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//   },
+//   author: {
+//     type: Sequelize.STRING,
+//   },
+// });
+// module.exports = story;
